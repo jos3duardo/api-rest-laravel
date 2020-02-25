a6e738985eddce0b2308e24866fbb8e3ee76fedf
jos3duardo <jos3duardolopes@gmail.com>
2020-02-25 15:45:20 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+criando  model, migration e controller de Products
+php artisan make:model Produtcs -mc     
+
+colocar campos na migration
+php artisan migrate       
+
+criando controller de user
+php artisan make:controller --resource User  
+
+criando uma validação para Products
+php artisan make:request ProductsRequest          
+
+Configurando passport passport
+composer require laravel/passport   
+php artisan migrate      
+php artisan passport:install  
+php artisan vendor:publish --tag=passport-components
+npm install
+npm run dev
+
+autenticação no laravel 6.x
+
+composer require laravel/ui --dev
+
+php artisan ui vue --auth
---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return User[]|\Illuminate\Database\Eloquent\Collection
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return User::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param User $user
+     * @return User
+     */
+    public function show(User $user)
+    {
+        return $user;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param User $user
+     * @return User
+     */
+    public function update(Request $request, User $user)
+    {
+        $user->update($request->all());
+
+        return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param User $user
+     * @return User
+     * @throws \Exception
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return $user;
+    }
+}
